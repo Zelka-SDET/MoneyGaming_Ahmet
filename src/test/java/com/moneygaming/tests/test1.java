ab0368d17fab7eb60a3f84a96d9899c3aaebe261
Zelka-SDET <genelhesap05@gmail.com>
2021-10-19 07:55:10 +0300

    I added my WebDriverFactory Utilitie class

--- src/test/java/com/moneygaming/tests/test1.java
+++ src/test/java/com/moneygaming/tests/test1.java
@@ -1,5 +1,7 @@
 package com.moneygaming.tests;
 
+import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,7 +10,10 @@ public class test1 {
 
     @BeforeMethod
     public void setUp(){
-
+        //opens me chrome driver
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //maximaze window
+        driver.manage().window().maximize();
 
     }
 
